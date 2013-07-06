230958419b572bc61fa0b1e2518c63b6a41f6bbd
Sergey <sergey.dvornikov@gmail.com>
2013-07-06 15:19:44 +0400

    Started Face class implementation

--- src/cyfn/rubics/Face.java
+++ src/cyfn/rubics/Face.java
@@ -1,15 +1,31 @@
 package cyfn.rubics;
 
 class Face {
+	private final int dimension;
+	private Piece[][] facePieces;
+	private Piece[][][] adgacentPieces;		//[depth][adjacent side][piece]
+	
+	Face(int dimension) {
+		this.dimension = dimension;
+		facePieces = new Piece[this.dimension][this.dimension];
+		adgacentPieces = new Piece[(int)(this.dimension/2)][4][this.dimension];
+		// TODO create facePices
+	}
+	
+	public void rotate(Direction dir, int turns, int depth) {
+		if(depth < 0 || depth > dimension/2 - 1) 
+			throw new IllegalArgumentException(String.format("Depth %i is illegal for %ix%i RubicsCube", depth,dimension,dimension));
+		if(turns < 0) 
+			throw new IllegalArgumentException("Number of turns must be positive");
+		
+		// TODO if depth=0 turn facePieces
+		// then turn adjacent pieces
+	}
+	
 	// TODO
 	/*
-	+ dimension property
-	constructor (dimension)
-	facePieces array[dim][dim]
-	adjacentPieces array[dim/2][4][dim] (depth,adj.side,piece) ?
-	rotate(direction,turns,depth)
 	rotateHalfTurn() - helpers...
-	rotateQuarterTurnCW(depth)
-	rotateQuarterTurnCCW(depth)
+	rotateQuarterTurnCW()
+	rotateQuarterTurnCCW()
 	*/
 }
