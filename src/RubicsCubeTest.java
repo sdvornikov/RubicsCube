f05a3a39b06faed457b659f8f4ab6049530c9046
Sergey <sergey.dvornikov@gmail.com>
2013-07-06 17:00:14 +0400

    Wrote a simple test

--- src/RubicsCubeTest.java
+++ src/RubicsCubeTest.java
@@ -1,13 +1,25 @@
 import cyfn.rubics.RubicsCube;
+import cyfn.rubics.Side;
 
 public class RubicsCubeTest {
 
 	public static void main(String[] args) {
-		// TODO create a test 3x3 cube
-		// Turn some faces
-		// get result state of the Cube
-		// verify results
+		RubicsCube cube3x3 = new RubicsCube(3);
+		for(Side side: Side.values()) {
+			System.out.println(side + " side");
+			printArray(cube3x3.getSidePicesColor(side));
+			System.out.println("");
+		}
 
+	}
+	
+	static void printArray(String[][] array) {
+		for(int i=0;i<array.length;i++) {
+			for(int j=0;j<array[0].length;j++) {
+				System.out.format("[%s]", array[i][j]);
+			}
+			System.out.println("");
+		}
 	}
 
 }
