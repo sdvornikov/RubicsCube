c4827fe969b1dfc5f070fe1af1c25d8dde908db6
Sergey <sergey.dvornikov@gmail.com>
2013-07-06 16:26:54 +0400

    More Face implementation

--- src/cyfn/rubics/Face.java
+++ src/cyfn/rubics/Face.java
@@ -3,29 +3,67 @@ package cyfn.rubics;
 class Face {
 	private final int dimension;
 	private Piece[][] facePieces;
-	private Piece[][][] adgacentPieces;		//[depth][adjacent side][piece]
+	private Piece[][][] adgacentPieces;		//[depth][adjacent side][piece]. Starts from [0][0] facePiece and continues clockwise
 	
-	Face(int dimension) {
+	Face(int dimension, String color) {
 		this.dimension = dimension;
 		facePieces = new Piece[this.dimension][this.dimension];
 		adgacentPieces = new Piece[(int)(this.dimension/2)][4][this.dimension];
-		// TODO create facePices
+		
+		for(int i=0;i<this.dimension;i++) {
+			for(int j=0;j<this.dimension;j++) {
+				facePieces[i][j] = new Piece(color);
+			}
+		}
+				
 	}
 	
-	public void rotate(Direction dir, int turns, int depth) {
+	public void setAdjacentPieces(Piece[][][] pieces) {
+		// allow only once ?
+		for(int i=0;i<adgacentPieces.length;i++) {
+			for(int j=0;j<adgacentPieces[0].length;j++) {
+				for(int k=0;k<adgacentPieces[0][0].length;k++) {
+					if (pieces[i][j][k] == null)
+						throw new NullPointerException("Cannot use null as adjacent piece");
+					adgacentPieces[i][j][k] = pieces[i][j][k];
+				}
+			}
+		}
+	}
+	
+	public void turn(Direction dir, int depth) {
 		if(depth < 0 || depth > dimension/2 - 1) 
 			throw new IllegalArgumentException(String.format("Depth %i is illegal for %ix%i RubicsCube", depth,dimension,dimension));
-		if(turns < 0) 
-			throw new IllegalArgumentException("Number of turns must be positive");
 		
-		// TODO if depth=0 turn facePieces
-		// then turn adjacent pieces
+		if(depth == 0) {
+			if(dir == Direction.CLOCKWISE)
+				turnFacePiecesCW();
+			else if(dir == Direction.COUNTERCLOCKWISE)
+				turnFacePiecesCCW();
+		}
+		
+		if(dir == Direction.CLOCKWISE)
+			turnAdjacentPiecesCW(depth);
+		else if(dir == Direction.COUNTERCLOCKWISE)
+			turnAdjacentPiecesCCW(depth);
 	}
 	
-	// TODO
+	// to transpose a face:
 	/*
-	rotateHalfTurn() - helpers...
-	rotateQuarterTurnCW()
-	rotateQuarterTurnCCW()
+	for n = 0 to N - 2
+		    for m = n + 1 to N - 1
+		        swap A(n,m) with A(m,n)
 	*/
+	private void turnFacePiecesCW() {
+		// TODO implement
+	}
+	private void turnFacePiecesCCW() {
+		// TODO implement		
+	}
+	private void turnAdjacentPiecesCW(int depth) {
+		// TODO implement
+	}
+	private void turnAdjacentPiecesCCW(int depth) {
+		// TODO implement
+	}	
 }
