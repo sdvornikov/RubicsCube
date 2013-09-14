51b75f359d16cbaa9fc37df0df14e5af689c1d8f
Sergey <sergey.dvornikov@gmail.com>
2013-09-14 19:36:43 +0400

    JUnit test stub

---
+++ test/cyfn/rubics/RubicsCubeTest.java
@@ -0,0 +1,59 @@
+package cyfn.rubics;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RubicsCubeTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testRubicsCube() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTurnFaceDirectionSideInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTurnFaceDirectionSide() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSidePicesColor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsSolved() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUndoLastTurn() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testScramble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTurnLog() {
+		fail("Not yet implemented");
+	}
+
+}
